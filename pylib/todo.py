92dc280ec44e732e78f959a1d7f024f14b6b809a
lilydjwg <lilydjwg@gmail.com>
2010-12-05 11:48:30 +0800

    todo 改用 SQLite。改名为 t

--- pylib/todo.py
+++ pylib/todo.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 # vim:fileencoding=utf-8
 
-from pickleddata import PData
-import random
+import sqlite3
+import os
+import sys
 
 class TODO:
   def __init__(self, fname):
-    self.p = PData(fname)
-    if self.p.data is None:
-      self.p.data = []
-    self.data = self.p.data
+    self.conn = sqlite3.connect(fname)
+    self.cursor = self.conn.cursor()
+    self.setup()
+
+  def setup(self):
+    sql = '''create table if not exists item (
+      id integer primary key autoincrement,
+      what text not null)'''
+    self.cursor.execute(sql)
+    sql = '''create table if not exists info (
+      name text primary key not null,
+      value text not null)'''
+    self.cursor.execute(sql)
+
+  def __del__(self):
+    self.cursor.close()
+    self.conn.commit()
+    self.conn.close()
 
   def __call__(self, argv):
     if not argv:
@@ -31,31 +46,60 @@ class TODO:
     for cmd in allcmd:
       print('\t%s\t%s' % cmd)
 
-  def do_add(self, what, priority=20):
-    '''添加新事务（可选优先级）'''
+  def do_edit(self):
+    '''使用文本编辑器编辑新事务'''
+    fname = '/tmp/todo_list'
+    editor = os.environ['EDITOR'] or 'vim'
     try:
-      priority = int(priority)
-    except ValueError:
-      raise CommandError('优先级应为整数')
+      os.system('%s %s' % (editor, fname))
+      what = open(fname).read().rstrip()
+      os.unlink(fname)
+    except IOError:
+      print('未找到编辑过的文件，中断。', file=sys.stderr)
+    self.do_add(what)
 
-    self.data.append((what, priority))
+  def do_add(self, what):
+    '''添加新事务'''
+    sql = '''insert into item (what) values (?)'''
+    self.cursor.execute(sql, (what,))
+
+  def do_last(self):
+    '''显示最后被选择的项'''
+    sql = '''select what from item where id in (
+      select value from info where name = 'last_id')'''
+    self.cursor.execute(sql)
+    for c in self.cursor:
+      what = c[0]
+    print(what)
+
+  def do_count(self):
+    '''事务总数'''
+    sql = '''select count(*) from item'''
+    self.cursor.execute(sql)
+    for c in self.cursor:
+      howmany = c[0]
+    print('共 %d 项事务' % howmany)
+
+  def do_del(self):
+    '''删除最后被选择的项'''
+    sql = '''delete from item where id in (
+      select value from info where name = 'last_id')'''
+    self.cursor.execute(sql)
 
   def do_get(self):
     '''选取一项事务'''
-    allpriority = sum(x[1] for x in self.data)
-    try:
-      choice = random.randrange(allpriority)
-    except ValueError:
+    sql = '''select id, what from item order by random() limit 1'''
+    self.cursor.execute(sql)
+    for c in self.cursor:
+      id, what = c
+      break
+    else:
       print('没有事务')
       return
-
-    a = 0
-    for i in self.data:
-      a += i[1]
-      if a > choice:
-        chosen = i[0]
-        break
-    print(chosen)
+    sql = '''insert or replace into info (name, value)
+      values ('last_id', '%d')''' % id
+    self.cursor.execute(sql)
+    print(what)
 
   def do_help(self):
     '''帮助信息'''
@@ -63,4 +107,3 @@ class TODO:
 
 class NoSuchCommand(LookupError): pass
 class CommandError(Exception): pass
-
