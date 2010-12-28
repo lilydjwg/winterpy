44f364ee3f8e211d5050c6bde5f27ec1e02e88b8
lilydjwg <lilydjwg@gmail.com>
2010-12-28 18:15:25 +0800

    decorator run_in_thread 更新

--- pylib/decorator.py
+++ pylib/decorator.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # vim:fileencoding=utf-8
 
-def run_in_thread_daemon(fn):
+def run_in_thread(daemon):
   import threading
+  fn = None
   def run(*k, **kw):
     t = threading.Thread(target=fn, args=k, kwargs=kw)
-    t.daemon = True
+    t.daemon = daemon
     t.start()
-  return run
-
-def run_in_thread(fn):
-  import threading
-  def run(*k, **kw):
-    t = threading.Thread(target=fn, args=k, kwargs=kw)
-    t.start()
-  return run
+  if isinstance(daemon, bool):
+    def wrapper(callback):
+      nonlocal fn
+      fn = callback
+      return run
+    return wrapper
+  else:
+    fn = daemon
+    daemon = False
+    return run
 
