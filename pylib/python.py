1511cbbd8c159e16b65ce4fa8f33cca304b09deb
lilydjwg <lilydjwg@gmail.com>
2011-02-03 00:27:47 +0800

    对 Python 脚本导入模块的分析

---
+++ pylib/python.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+import os
+
+def mymodsImported(scriptfile):
+  '''导入的模块哪些是通过环境变量找到的？'''
+  try:
+    dirs = os.getenv('PYTHONPATH').split(':')
+  except AttributeError:
+    return []
+
+  if not dirs:
+    return []
+
+  from modulefinder import ModuleFinder
+  finder = ModuleFinder()
+  finder.run_script(scriptfile)
+
+  def filterdir(mod):
+    file = mod.__file__
+    if not file:
+      return False
+    for i in dirs:
+      if file.startswith(i):
+        return True
+    return False
+
+  return [m for m in finder.modules.values() if filterdir(m)]
