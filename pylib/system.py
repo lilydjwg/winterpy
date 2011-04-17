ddaba4180cb365b328a6119554d2f39f6c855f28
lilydjwg <lilydjwg@gmail.com>
2011-04-17 23:53:03 +0800

    CPU 温度监控

---
+++ pylib/system.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+'''
+系统工具，大多是调用外部程序
+'''
+
+import subprocess
+
+def getCPUTemp():
+  status, output = subprocess.getstatusoutput('sensors')
+  if status != 0:
+    raise SubprocessError("failed to execute `sensors'")
+  output = output.split('\n')
+  for l in output:
+    if l.startswith('CPU Temperature:'):
+      end = l.find('°')
+      return float(l[16:end])
+  raise SubprocessError('CPU Temperature not available')
+
+def setMaxCPUFreq(freq):
+  cmd = ['cpufreq-set', '--max', str(freq)]
+  retcode = subprocess.call(cmd)
+  if retcode != 0:
+    raise SubprocessError()
+
+class SubprocessError(SystemError): pass
