a5bed853369947cb3104b5c6661668930987e676
lilydjwg <lilydjwg@gmail.com>
2011-06-16 21:50:48 +0800

    WingDNS 数据更新脚本
    * 已知问题：无法通过 cookie 保持登录状态

---
+++ pylib/wingdns.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+import re
+from urllib.parse import urlencode
+from getpass import getpass
+from collections import defaultdict
+
+from lxml.html import parse, submit_form
+
+from httpsession import Session
+
+idAndTypeParser = re.compile(r"inputUseable\(this, '(?P<hostid>\d+)','(?P<type>\w+)'\)")
+authCookiePageURL = re.compile(r'(?<=src=")[^"]+(?=")')
+
+def encodeHost(func):
+  def wrapper(self, record):
+    record = record.copy()
+    record['host'] = urlencode(record['host'])
+    func(record)
+  return func
+
+class WingDNSError(Exception): pass
+
+class WingDNS(Session):
+  UserAgent = 'IP updater'
+  def login(self, username, password):
+    doc = parse(self.request('http://www.wingdns.com/')).getroot()
+    form = doc.forms[0]
+    form.fields['username'] = username.encode()
+    form.fields['pwd'] = password.encode()
+    form.fields['checkbox'] = b'checkbox'
+    r = submit_form(form, open_http=self.http_open).read().decode()
+    for u in authCookiePageURL.findall(r):
+      self.request(u).read()
+
+  def getAdminConsole(self, id):
+    res = self.request('http://www.wingdns.com/records.php?id=%s' % id).read().decode('cp936')
+    if res.startswith('0|'):
+      raise WingDNSError(res.split('|', 1)[-1])
+    doc = parse(res).getroot()
+    zone = doc.cssselect('#domain_name')[0].text
+    self.records = defaultdict(list)
+    self.records['zone'] = zone
+    for i in doc.cssselect('[value=修改]'):
+      m = idAndTypeParser.match(i.get('onclick'))
+      if m:
+        type = m.group('type')
+        r = {}
+        r['hostid'] = m.group('hostid')
+        tr = i.getparent().getparent()
+        r['host'] = tr.cssselect('#sub_domain')[0].value
+        r['data'] = tr.cssselect('#value')[0].value
+        r['mx'] = tr.cssselect('#mx')[0].value
+        self.records[type].append(r)
+
+  @encodeHost
+  def add(self, record):
+    url = 'http://www.wingdns.com/ajax_records.php?action=add&zone={zone}&num={type}&host={host}&type=0&data={data}&ttl=60&mx={mx}'.format(zone=self.records['zone'], **record)
+    r = self.request(url)
+    status, hostid = r.read().decode().split('|', 1)
+    if status == '1':
+      return hostid
+    else:
+      raise ValueError(hostid)
+
+  @encodeHost
+  def modi(self, record):
+    url = 'http://www.wingdns.com/ajax_records.php?action=modi&zone={zone}&hostid={hostid}&host={host}&type=0&data={data}&ttl=60&mx={mx}'.format(zone=self.records['zone'], **record)
+    r = self.request(url)
+    status = r.read().decode().split('|', 1)[0]
+    if status != '1':
+      raise ValueError(hostid)
+
+  @encodeHost
+  def del_(self, record):
+    url = 'http://www.wingdns.com/ajax_records.php?action=del&hostid={hostid}&zone={zone}'.format(zone=self.records['zone'], **record)
+    r = self.request(url)
+    status = r.read().decode().split('|', 1)[0]
+    if status != '1':
+      raise ValueError(hostid)
+
+  def http_open(self, method, url, values):
+    if method == 'GET':
+      if '?' in url:
+          url += '&'
+      else:
+          url += '?'
+      url += urlencode(values)
+      data = None
+    else:
+      data = urlencode(values)
+    return self.request(url, data)
+
