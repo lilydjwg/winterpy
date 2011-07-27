66bb7fac355c4ea6d90c1b6e3d579501befe5bc0
lilydjwg <lilydjwg@gmail.com>
2011-07-27 12:09:10 +0800

    checktime 不再依赖 greenlet

--- pylib/checktime.py
+++ pylib/checktime.py
@@ -6,29 +6,26 @@ check if any file in a list has been modified or got deleted
 '''
 
 import os
-import greenlet
- 
-class TimeChecker(greenlet.greenlet):
+
+class TimeChecker:
   '''
-  Initialed with a list of file paths. Later ``switch()`` call will return a changed file list.
+  Initialed with a list of file paths. Later ``check()`` call will return a changed file list.
   passing in another list to change the file list to be monitored.
 
   you can access the file list (a tuple) by its property ``filelist``.
   '''
   def __init__(self, filelist):
-    super().__init__()
     # outside may not changed this accidentally
     self.filelist = tuple(filelist)
     self.modifyTimes = {}
-    self.switch()
- 
-  def run(self):
-    while True:
-      result = self.updateTime()
-      newlist = self.parent.switch(result)
-      if newlist:
-        self.modifyTimes = {}
-        self.filelist = tuple(newlist)
+    self.check()
+
+  def check(self, newlist=None):
+    result = self.updateTime()
+    if newlist:
+      self.modifyTimes = {}
+      self.filelist = tuple(newlist)
+    return result
 
   def updateTime(self):
     modifyTimes = self.modifyTimes
