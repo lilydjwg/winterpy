0b159f2421d5a451f6a80fb3f4b1ebadb4f0484d
lilydjwg <lilydjwg@gmail.com>
2011-07-27 13:09:27 +0800

    checktime 使用 frozenset 取代 tuple

--- pylib/checktime.py
+++ pylib/checktime.py
@@ -2,21 +2,21 @@
 # vim:fileencoding=utf-8
 
 '''
-check if any file in a list has been modified or got deleted
+check if any file in a set has been modified or got deleted
 '''
 
 import os
 
 class TimeChecker:
   '''
-  Initialed with a list of file paths. Later ``check()`` call will return a changed file list.
-  passing in another list to change the file list to be monitored.
+  Initialed with a set of file paths. Later ``check()`` call will return a changed file list.
+  passing in another set to change the file set to be monitored.
 
-  you can access the file list (a tuple) by its property ``filelist``.
+  you can access the file set (a frozenset) by its property ``fileset``.
   '''
-  def __init__(self, filelist):
+  def __init__(self, fileset):
     # outside may not changed this accidentally
-    self.filelist = tuple(filelist)
+    self.fileset = frozenset(fileset)
     self.modifyTimes = {}
     self.check()
 
@@ -24,13 +24,13 @@ class TimeChecker:
     result = self.updateTime()
     if newlist:
       self.modifyTimes = {}
-      self.filelist = tuple(newlist)
+      self.fileset = frozenset(newlist)
     return result
 
   def updateTime(self):
     modifyTimes = self.modifyTimes
     modified = []
-    for path in self.filelist:
+    for path in self.fileset:
       try:
         modify_time = os.stat(path).st_mtime
       except OSError:
