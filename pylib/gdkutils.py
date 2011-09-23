206e916a1ee70990348d2ed0f5dc582709277109
lilydjwg <lilydjwg@gmail.com>
2011-09-23 16:53:45 +0800

    add gdkutils: get_screen_size and screenshot

---
+++ pylib/gdkutils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vim:fileencoding=utf-8
+
+'''
+utils using GDK
+'''
+
+import mimetypes
+
+from gi.repository import Gdk
+
+def get_screen_size():
+  screen = Gdk.Screen.get_default()
+  return screen.width(), screen.height()
+
+def screenshot(filename, rect=None, filetype=None):
+  screen = Gdk.Screen.get_default()
+  if rect is None:
+    rect = (0, 0, screen.width(), screen.height())
+  if filetype is None:
+    t = mimetypes.guess_type(filename)[0]
+    if t is None:
+      raise ValueError('cannot guess filetype for filename: %s' % filename)
+    filetype = t.split('/')[1]
+
+  rootwin = screen.get_root_window()
+  pixbuf = Gdk.pixbuf_get_from_window(rootwin, *rect)
+  pixbuf.savev(filename, filetype, (), ())
