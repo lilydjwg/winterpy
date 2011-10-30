85393e7aa14ea9c114a00a259e1c2758832bf930
lilydjwg <lilydjwg@gmail.com>
2011-10-30 18:04:54 +0800

    xauto

--- pylib/xauto.py
+++ pylib/xauto.py
@@ -11,18 +11,36 @@ import gdkutils
 class XAuto:
   _screensize = None
 
-  def __init__(self, tmp_img='/dev/shm/tmp%d.png' % os.getpid(), default_threshold=0.7):
+  def __init__(self, tmp_img='/dev/shm/tmp%d.png' % os.getpid(),
+    default_threshold=0.7, default_rect=None):
     self.d = X.Display()
     self.tmp_img = tmp_img
     self.default_threshold = default_threshold
+    self.default_rect = default_rect
 
-  def find_and_click(self, *args, **kwargs):
+  def find_and_click(self, *args, back=False, **kwargs):
     pos = self.find(*args, **kwargs)
     if pos:
-      self.click_and_back(pos)
-      return True
-    else:
-      return False
+      if back:
+        self.click_and_back(pos)
+      else:
+        self.click(pos)
+    return pos
+
+  def find_and_moveto(self, *args, **kwargs):
+    pos = self.find(*args, **kwargs)
+    if pos:
+      self.moveto(pos)
+    return pos
+
+  def click(self, pos, button=X.LEFT_BUTTON):
+    d = self.d
+    d.motion(pos)
+    d.button(button)
+    d.flush()
+
+  def wait(self, seconds):
+    sleep(seconds)
 
   def click_and_back(self, pos, button=X.LEFT_BUTTON):
     d = self.d
@@ -32,11 +50,21 @@ class XAuto:
     d.motion(old_pos)
     d.flush()
 
-  def find(self, img, threshold=None, rect=None, repeat=0, interval=0.2):
+  def moveto(self, pos):
+    d = self.d
+    d.motion(pos)
+    d.flush()
+
+  def key(self, keyname):
+    d = self.d
+    d.key(keyname)
+    d.flush()
+
+  def find(self, img, threshold=None, rect=None, repeat=1, interval=0.2):
     if isinstance(img, str):
       img = Image(img)
     if rect is None:
-      rect = (0, 0) + self.screensize
+      rect = self.default_rect or (0, 0) + self.screensize
     if threshold is None:
       threshold = self.default_threshold
     tmp_img = self.tmp_img
