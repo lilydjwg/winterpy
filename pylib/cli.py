7ce2bca859a092216f7bd3175ea10d6e26c5a0fd
lilydjwg <lilydjwg@gmail.com>
2012-03-25 14:14:17 +0800

    删除 delbroken；zsh 中使用 *(-@) 即可

--- pylib/cli.py
+++ pylib/cli.py
@@ -15,13 +15,6 @@ def findbroken(p):
 
   return ret
 
-def delbroken(p=path('.')):
-  '''删掉该目录下断掉的软链接'''
-  l = [x for x in p.list() if not x.exists()]
-  for i in l:
-    i.unlink()
-    print('已删除', i)
-
 def repl(local, histfile=None, banner=None):
   import readline
   import rlcompleter
