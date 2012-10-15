2b12d8010101895a6b384200f41e32d6979d1d84
lilydjwg <lilydjwg@gmail.com>
2012-10-15 23:47:00 +0800

    mytornado moved to a package

--- pylib/mytornado/http.py
+++ pylib/mytornado/http.py
@@ -22,6 +22,8 @@ from tornado.web import HTTPError, RequestHandler, asynchronous, GZipContentEnco
 import tornado.escape
 import tornado.httpserver
 
+from .util import FileEntry
+
 logger = logging.getLogger(__name__)
 _legal_range = re.compile(r'bytes=(\d*)-(\d*)$')
 
@@ -58,26 +60,6 @@ class ErrorHandlerMixin:
         "message": httpclient.responses[status_code],
         "err": err_msg,
       })
-
-class FileEntry:
-  '''For ``StaticFileHandler`` with directory index enabled'''
-  isdir = False
-  def __init__(self, path, file):
-    st = os.stat(os.path.join(path, file))
-    self.time = datetime.datetime.fromtimestamp(st[stat.ST_MTIME])
-    self.name = file
-    self.filename = file
-    if stat.S_ISDIR(st[stat.ST_MODE]):
-      self.isdir = True
-      self.filename += '/'
-    self.size = st[stat.ST_SIZE]
-
-  def __lt__(self, another):
-    if self.isdir and not another.isdir:
-      return True
-    if not self.isdir and another.isdir:
-      return False
-    return self.name < another.name
 
 class StaticFileHandler(RequestHandler):
   """A simple handler that can serve static content from a directory.
