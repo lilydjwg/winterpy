e465f3c91138683f079b922865180b9aac96e70b
lilydjwg <lilydjwg@gmail.com>
2013-08-20 23:44:49 +0800

    mailutils: fix header decoding

--- pylib/mailutils.py
+++ pylib/mailutils.py
@@ -7,10 +7,10 @@ def decode_multiline_header(s):
   ret = []
 
   for b, e in header.decode_header(re.sub(r'\n\s+', ' ', s)):
-    if e is None:
-      e = 'ascii'
-    elif e.lower() == 'gb2312':
-      e = 'gb18030'
-    ret.append(b.decode(e))
+    if e:
+      if e.lower() == 'gb2312':
+        e = 'gb18030'
+      b = b.decode(e)
+    ret.append(b)
 
   return ''.join(ret)
