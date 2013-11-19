e7269f782b7dc9d033a532623c8b607b6570b09d
lilydjwg <lilydjwg@gmail.com>
2013-11-19 20:48:27 +0800

    xiami: better organization; add findBestLrc func

--- pylib/xiami.py
+++ pylib/xiami.py
@@ -7,49 +7,61 @@ from htmlutils import parse_document_from_requests
 
 SongInfo = namedtuple('SongInfo', 'sid name href artist album info')
 
-def getSession():
-  s = requests.Session()
-  s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0'
-  return s
+class Xiami:
+  _session = None
 
-def search(q, session=None):
-  if session is None:
-    session = getSession()
+  def __init__(self, session=None):
+    self._session = session
 
-  url = 'http://www.xiami.com/search?key=' + q
-  doc = parse_document_from_requests(url, session)
-  rows = doc.xpath('//table[@class="track_list"]//tr')[1:]
-  ret = []
-  for tr in rows:
-    # 没有 target 属性的是用于展开的按钮
-    names = tr.xpath('td[@class="song_name"]/a[@target]')
-    if len(names) == 2:
-      info = names[1].text_content()
-    else:
-      info = None
-    name = names[0].text_content()
-    href = names[0].get('href')
+  @property
+  def session(self):
+    if not self._session:
+      s = requests.Session()
+      s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:20.0) ' \
+          'Gecko/20100101 Firefox/20.0'
+      self._session = s
+    return self._session
 
-    # '/text()' in XPath get '.text', not '.text_content()'
-    artist = tr.xpath('td[@class="song_artist"]/a')[0].text_content().strip()
-    album = tr.xpath('td[@class="song_album"]/a')[0].text_content().strip()
-    album = album.lstrip('《').rstrip('》')
+  def search(self, q):
+    url = 'http://www.xiami.com/search?key=' + q
+    doc = parse_document_from_requests(url, self.session)
+    rows = doc.xpath('//table[@class="track_list"]//tr')[1:]
+    ret = []
+    for tr in rows:
+      # 没有 target 属性的是用于展开的按钮
+      names = tr.xpath('td[@class="song_name"]/a[@target]')
+      if len(names) == 2:
+        info = names[1].text_content()
+      else:
+        info = None
+      name = names[0].text_content()
+      href = names[0].get('href')
 
-    sid = href.rsplit('/', 1)[-1]
-    song = SongInfo(sid, name, href, artist, album, info)
-    ret.append(song)
+      # '/text()' in XPath get '.text', not '.text_content()'
+      artist = tr.xpath('td[@class="song_artist"]/a')[0].text_content().strip()
+      album = tr.xpath('td[@class="song_album"]/a')[0].text_content().strip()
+      album = album.lstrip('《').rstrip('》')
 
-  return ret
+      sid = href.rsplit('/', 1)[-1]
+      song = SongInfo(sid, name, href, artist, album, info)
+      ret.append(song)
 
-def getLyricFor(sid, session=None):
-  if session is None:
-    session = getSession()
+    return ret
 
-  url = 'http://www.xiami.com/song/playlist/id/%s/object_name/default/object_id/0' % sid
-  r = session.get(url)
-  doc = fromstring(r.content)
-  lyric_url = doc.xpath('//xspf:lyric/text()', namespaces={'xspf': 'http://xspf.org/ns/0/'})[0]
-  # pic_url = doc.xpath('//xspf:pic/text()', namespaces={'xspf': 'http://xspf.org/ns/0/'})[0]
+  def getLyricFor(self, sid):
+    url = 'http://www.xiami.com/song/playlist/id/%s/object_name/default/object_id/0' % sid
+    r = self.session.get(url)
+    doc = fromstring(r.content)
+    lyric_url = doc.xpath('//xspf:lyric/text()', namespaces={'xspf': 'http://xspf.org/ns/0/'})[0]
+    # pic_url = doc.xpath('//xspf:pic/text()', namespaces={'xspf': 'http://xspf.org/ns/0/'})[0]
 
-  r = session.get(lyric_url)
-  return r.text
+    r = self.session.get(lyric_url)
+    return r.text
+
+  def findBestLrc(self, q):
+    results = self.search(q)
+    for song in results:
+      lyric = self.getLyricFor(song.sid)
+      if lyric.count('[') < 5:
+        continue
+      return lyric
