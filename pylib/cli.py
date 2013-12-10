98ab2aa676860a3ae29333c2afdc628e45c569ee
lilydjwg <lilydjwg@gmail.com>
2013-12-10 18:04:11 +0800

    cli: add repl_py27 for awkward Python 2.x unicode display

--- pylib/cli.py
+++ pylib/cli.py
@@ -1,7 +1,11 @@
 # vim:fileencoding=utf-8
 # This file is in the Public Domain
 
-'''一些在命令行上使用的便捷函数'''
+'''
+Convenient functions for command-line use.
+
+Python 2 & 3
+'''
 
 import os
 
@@ -25,3 +29,29 @@ def repl_reset_stdin(*args, **kwargs):
   os.dup2(fd, 0)
   os.close(fd)
   repl(*args, **kwargs)
+
+def repl_py27(local, *args, **kwargs):
+  '''Fix unicode display in Python 2.x by filtering through the ascii2uni program'''
+  import subprocess, sys, time
+  p = subprocess.Popen(['ascii2uni', '-qa7'],
+                       stdin=subprocess.PIPE, preexec_fn=os.setpgrp)
+
+  def displayfunc(value):
+    if value is None:
+      local['_'] = None
+      return
+
+    value = repr(value)
+    if value.find(r'\x') != -1:
+      p.stdin.write(value+'\n')
+      time.sleep(0.01)
+    else:
+      print(value)
+    local['_'] = value
+
+  sys.displayhook = displayfunc
+  try:
+    repl(local, *args, **kwargs)
+  finally:
+    p.stdin.close()
+    p.wait()
