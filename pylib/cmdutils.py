9fc184cc169779839d83c082d714103f51fd328b
lilydjwg <lilydjwg@gmail.com>
2013-12-16 00:06:54 +0800

    cmdutils: add check_mediafile()

--- pylib/cmdutils.py
+++ pylib/cmdutils.py
@@ -10,3 +10,17 @@ from functools import lru_cache
 @lru_cache(maxsize=20)
 def lookupip(ip, cmd='cip'):
   return subprocess.getoutput(subprocess.list2cmdline([cmd, ip])).replace('CZ88.NET', '').strip() or '-'
+
+def check_mediafile(file):
+  '''intergrity check with ffmpeg
+
+  also return ``False`` when reading the file fails
+
+  From http://superuser.com/a/100290/40869
+  '''
+
+  p = subprocess.Popen(
+    ['ffmpeg', '-v', 'error', '-i', file, '-f', 'null', '-'],
+    stderr = subprocess.PIPE)
+  _, e = p.communicate()
+  return not bool(e.strip())
