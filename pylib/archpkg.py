d6af7cb8f1a0dc537e3c36fca9ae6c0aadf52e3b
lilydjwg <lilydjwg@gmail.com>
2014-03-23 14:58:47 +0800

    remove archpkgfinddups; it's deprecated and has bugs

--- pylib/archpkg.py
+++ pylib/archpkg.py
@@ -27,10 +27,3 @@ def trimext(name, num=1):
   for i in range(num):
     name = os.path.splitext(name)[0]
   return name
-
-def finddups(pkgs, n=1):
-  ret = defaultdict(list)
-  for f in pkgs:
-    name, ver, build, arch = PkgNameInfo.parseFilename(os.path.split(f)[1])
-    ret[name].append('%s-%s' % (ver, build))
-  return {k: sorted(v) for k, v in ret.items() if len(v) > n}
