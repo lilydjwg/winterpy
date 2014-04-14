lilydjwg/winterpy

commit e3f433a6c511c49007d8388c8eb7dcea62d73f42

[fetchtitle] cancel timeout when exception happens

pylib/mytornado/fetchtitle.py
@@ -269,11 +269,6 @@ class TitleFetcher:
     if url_finders is not None:
       self._url_finders = url_finders
 
-    self.start_time = self.io_loop.time()
-    self._timeout = self.io_loop.add_timeout(
-      self.timeout + self.start_time,
-      self.on_timeout,
-    )
     self.origurl = url
     self.url_visited = []
     if run_at_init:
@@ -283,7 +278,15 @@ class TitleFetcher:
     if self.url_visited:
       raise Exception("can't run again")
     else:
-      self.new_url(self.origurl)
+      self.start_time = self.io_loop.time()
+      self._timeout = self.io_loop.add_timeout(
+        self.timeout + self.start_time,
+        self.on_timeout,
+      )
+      try:
+        self.new_url(self.origurl)
+      finally:
+        self.io_loop.remove_timeout(self._timeout)
 
   def on_timeout(self):
     self.run_callback(Timeout)

commit 000a942f4e5a22ce538c3a0f4c3d13b00d6417c9

fetchtitle: only cancel timeout when exception happens
This is a bug introduced by commit e3f433a6c511c49007d

pylib/mytornado/fetchtitle.py
@@ -305,10 +305,12 @@ class TitleFetcher:
       )
       try:
         self.new_url(self.origurl)
-      finally:
+      except:
         self.io_loop.remove_timeout(self._timeout)
+        raise
 
   def on_timeout(self):
+    logger.debug('request timed out')
     self.run_callback(Timeout)
 
   def parse_url(self, url):
@@ -585,7 +587,7 @@ def test():
     'http://img.vim-cn.com/22/cd42b4c776c588b6e69051a22e42dabf28f436', # image with length
     'https://github.com/m13253/titlebot/blob/master/titlebot.py_', # 404
     'http://lilydjwg.is-programmer.com/admin', # redirection
-    'http://twitter.com', # timeout
+    'http://twitter.com', # connect timeout
     'http://www.wordpress.com', # reset
     'http://jquery-api-zh-cn.googlecode.com/svn/trunk/xml/jqueryapi.xml', # xml
     'http://lilydjwg.is-programmer.com/user_files/lilydjwg/config/avatar.png', # PNG