823490529df612b1d73642ae07d58fb350196c82
lilydjwg <lilydjwg@gmail.com>
2014-04-22 16:42:45 +0800

    archpkg: add get_abs_pkgbuild_with_bash

--- pylib/archpkg.py
+++ pylib/archpkg.py
@@ -44,16 +44,29 @@ echo ${pkgname[*]}''' % PKGBUILD
       ret, ['bash'], output)
   return output.split()
 
-def get_aur_pkgbuild_with_bash(name):
-  script = '''\
-. /usr/lib/yaourt/util.sh
-. /usr/lib/yaourt/aur.sh
-aur_get_pkgbuild '%s' ''' % name
+def _run_bash(script):
   p = subprocess.Popen(['bash'], stdin=subprocess.PIPE)
   p.communicate(script.encode('latin1'))
   ret = p.wait()
   if ret != 0:
     raise subprocess.CalledProcessError(
       ret, ['bash'])
+
+def get_aur_pkgbuild_with_bash(name):
+  script = '''\
+. /usr/lib/yaourt/util.sh
+. /usr/lib/yaourt/aur.sh
+aur_get_pkgbuild '%s' ''' % name
+  _run_bash(script)
+
+def get_abs_pkgbuild_with_bash(name):
+  script = '''\
+. /usr/lib/yaourt/util.sh
+. /usr/lib/yaourt/abs.sh
+parse_pacman_conf
+arg=$(pacman -Sp --print-format '%%r/%%n' '%s')
+RSYNCOPT="$RSYNCOPT -O"
+abs_get_pkgbuild "$arg" ''' % name
+  _run_bash(script)
 
 pkgfile_pat = re.compile(r'(?:^|/).+-[^-]+-\d+-(?:\w+)\.pkg\.tar\.xz$')
