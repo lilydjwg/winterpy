9e473ea7af4190cce449ae32bf7d04feb2af6e47
lilydjwg <lilydjwg@gmail.com>
2014-05-10 18:06:55 +0800

    mailutils: 添加 assemble_mail 函数

--- pylib/mailutils.py
+++ pylib/mailutils.py
@@ -2,6 +2,11 @@
 
 import re
 from email import header
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+addr_re = re.compile(r'(.*?)\s+(<[^>]+>)($|,\s*)')
 
 def decode_multiline_header(s):
   ret = []
@@ -16,3 +21,35 @@ def decode_multiline_header(s):
     ret.append(b)
 
   return ''.join(ret)
+
+def assemble_mail(subject, to, from_, html=None, text=None):
+  if html is None and text is None:
+    raise TypeError('no message given')
+
+  if html:
+    html = MIMEText(html, 'html', 'utf-8')
+  if text:
+    text = MIMEText(text, 'plain', 'utf-8')
+
+  if html and text:
+    msg = MIMEMultipart('alternative', _subparts = [text, html])
+  else:
+    msg = html or text
+
+  msg['Subject'] = encode_header(subject)
+  msg['From'] = encode_header_address(from_)
+  msg['To'] = encode_header_address(to)
+
+  return msg
+
+def encode_header_address(s):
+  return addr_re.sub(_addr_submatch, s)
+
+def encode_header(s):
+  return Header(s, 'utf-8').encode() if not eight_bit_clean(s) else s
+
+def _addr_submatch(m):
+  return encode_header(m.group(1)) + ' ' + m.group(2) + m.group(3)
+
+def eight_bit_clean(s):
+  return all(ord(c) < 128 for c in s)
