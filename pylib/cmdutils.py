decdd595d9158557b43d13d0e213ac155dea8920
lilydjwg <lilydjwg@gmail.com>
2014-06-15 16:07:25 +0800

    cmdutils: add ping function

--- pylib/cmdutils.py
+++ pylib/cmdutils.py
@@ -4,8 +4,10 @@
 call external tools to do things.
 '''
 
+import re
 import subprocess
 from functools import lru_cache
+from collections import namedtuple
 
 @lru_cache(maxsize=20)
 def lookupip(ip, cmd='cip'):
@@ -24,3 +26,21 @@ def check_mediafile(file):
     stderr = subprocess.PIPE)
   _, e = p.communicate()
   return not bool(e.strip())
+
+PingResult = namedtuple('PingResult', 'loss avg mdev')
+
+def ping(host, *, count=4):
+  p = subprocess.Popen(
+    ['ping', '-c', str(count), host],
+    stdout = subprocess.PIPE)
+  out, _ = p.communicate()
+  out = out.decode()
+  _, loss, stat, _ = out.rsplit('\n', 3)
+  loss = re.findall(r'\d+(?=%)', loss)[0]
+  loss = float(loss) / 100
+  if stat:
+    stat = stat.split()[3]
+    _, avg, _, mdev = stat.split('/')
+  else:
+    avg = mdev = -1
+  return PingResult(loss, float(avg), float(mdev))
