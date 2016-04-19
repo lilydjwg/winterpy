ee0788508db0de9719a66f57f53fefb44d7d4542
依云 <lilydjwg@gmail.com>
2016-04-19 15:17:45 +0800

    mwutils: add move_category function

---
+++ pylib/mwutils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import re
+
+def move_category(site, old, new):
+  cat = site.pages['Category:' + old]
+  pages = cat.members()
+  cat_re = re.compile(r'(\[\[:?(?:分类|Category):)%s(\]\])' % re.escape(old))
+  for p in pages:
+    old_text = p.text()
+    text = cat_re.sub(r'\1%s\2' % new.replace('\\', r'\\'), old_text)
+    if text == old_text:
+      continue
+    p.save(text, '分类重命名：[[Category:%s]] -> [[Category:%s]]' % (
+      old, new))
