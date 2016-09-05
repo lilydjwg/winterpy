c912aa128123dd0de1521a3a7fb71e5e38a54a76
依云 <lilydjwg@gmail.com>
2016-09-05 15:41:54 +0800

    netutils: add get_interface_names and handle errors for get_my_ip

--- pylib/netutils.py
+++ pylib/netutils.py
@@ -5,8 +5,24 @@ import struct
 def get_my_ip(ifname):
   ifname = ifname.encode('ascii')
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
   # 0x8915 是 SIOCGIFADDR
-  ip = socket.inet_ntoa(
-    fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
+  try:
+    result = fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24]
+  except OSError as e:
+    if e.errno == 99: # Cannot assign requested address
+      return None
+    else:
+      raise
+
+  ip = socket.inet_ntoa(result)
   return ip
 
+def get_interface_names():
+  ret = []
+  with open('/proc/net/dev') as f:
+    for line in f:
+      first = line.split(None, 1)[0]
+      if first.endswith(':'):
+        ret.append(first[:-1])
+  return ret
