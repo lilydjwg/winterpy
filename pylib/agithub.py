187f336240eec390c65671f76d14095a95b3697c
lilydjwg <lilydjwg@gmail.com>
2017-12-30 16:07:53 +0800

    agithub: assign issues and search by email

--- pylib/agithub.py
+++ pylib/agithub.py
@@ -1,8 +1,12 @@
 import datetime
 import json
 import weakref
+import asyncio
+import logging
 
 import aiohttputils
+
+logger = logging.getLogger(__name__)
 
 def parse_datetime(s):
   dt = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ')
@@ -35,11 +39,17 @@ class GitHub(aiohttputils.ClientBase):
       h.setdefault('Content-Type', 'application/json')
       kwargs['data'] = data
 
-    res = await self.request(path, method=method, *args, **kwargs)
-    j = await res.json()
-    if 'message' in j:
-      raise GitHubError(j['message'], j['documentation_url'], res.status)
-    return j
+    for _ in range(3):
+      res = await self.request(path, method=method, *args, **kwargs)
+      j = await res.json()
+      if 'message' in j:
+        if res.status == 403 and int(res.headers.get('X-RateLimit-Remaining')) == 0:
+          reset = int(res.headers.get('X-RateLimit-Reset')) - asyncio.time() + 1
+          logger.warn('rate limited; sleeping for %ds: %s', reset, j['message'])
+          await asyncio.sleep(reset)
+          continue
+        raise GitHubError(j['message'], j['documentation_url'], res.status)
+      return j
 
   async def get_repo_issues(self, repo, *, state='open', labels=''):
     params = {'state': state}
@@ -67,10 +77,18 @@ class GitHub(aiohttputils.ClientBase):
     issue = await self.api_request(f'/repos/{repo}/issues', data = data)
     return Issue(issue, self)
 
+  async def find_login_by_email(self, email):
+    j = await self.api_request(f'/search/users?q={email}')
+    try:
+      return j['items'][0]['login']
+    except IndexError:
+      raise LookupError(email)
+
 class Issue:
   def __init__(self, data, gh):
     self.gh = weakref.proxy(gh)
     self._data = data
+    self.body = data['body']
     self.number = data['number']
     self.title = data['title']
     self.labels = [x['name'] for x in data['labels']]
@@ -84,6 +102,12 @@ class Issue:
     if not isinstance(labels, (list, tuple)):
       raise TypeError('labels should be a list')
     return await self.gh.api_request(f'{self._api_url}/labels', data = labels)
+
+  async def assign(self, assignees):
+    if not isinstance(assignees, (list, tuple)):
+      raise TypeError('assignees should be a list')
+    payload = {'assignees': assignees}
+    return await self.gh.api_request(f'{self._api_url}/assignees', data = payload)
 
   async def close(self):
     return await self.gh.api_request(f'{self._api_url}', method = 'patch',
