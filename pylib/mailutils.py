af4e6f9be13d4bc724cc68eda74d41ab608ba165
lilydjwg <lilydjwg@gmail.com>
2018-07-23 19:09:32 +0800

    mailutils: remove mailextract and put most of it into a save_html_mail function

--- pylib/mailutils.py
+++ pylib/mailutils.py
@@ -5,6 +5,7 @@ import datetime
 import codecs
 import smtplib
 from email import header
+import email.header
 from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -32,15 +33,18 @@ def get_datetime(m):
   # convert to local time
   return datetime.datetime.fromtimestamp(t.timestamp())
 
-def decode_payload(m):
+def decode_payload(m, *, binary=False):
   p = m.get_payload()
   enc = m['Content-Transfer-Encoding']
   ctype = m['Content-Type']
-  charset = get_charset_from_ctype(ctype) or 'utf-8'
   if enc == '8bit':
     return p
   else:
-    return codecs.decode(p.encode(), enc).decode(charset)
+    data = codecs.decode(p.encode(), enc)
+    if not binary:
+      charset = get_charset_from_ctype(ctype) or 'utf-8'
+      data = data.decode(charset)
+    return data
 
 def assemble_mail(subject, to, from_, html=None, text=None):
   if html is None and text is None:
@@ -71,6 +75,19 @@ def encode_header_address(s):
 def encode_header(s):
   return Header(s, 'utf-8').encode() if not eight_bit_clean(s) else s
 
+def decode_header(h):
+  var = email.header.decode_header(h)[0]
+  charset = var[1] or 'ascii'
+  if charset.lower() == 'gb2312': #fxxk
+    charset = 'gb18030'
+  try:
+    var = var[0].decode(charset)
+  except AttributeError:
+    var = var[0]
+  except LookupError:
+    var = var[0].decode('utf-8', errors='replace')
+  return var
+
 def _addr_submatch(m):
   return encode_header(m.group(1)) + ' ' + m.group(2) + m.group(3)
 
@@ -95,3 +112,73 @@ def sendmail(mail):
   s.connect()
   s.send_message(mail)
   s.quit()
+
+def save_html_mail(msg):
+  import os
+  import tempfile
+
+  basedir = tempfile.mkdtemp()
+
+  def save_file(fname, content):
+    fname = os.path.join(basedir, fname)
+    if isinstance(content, str):
+      f = open(fname, 'w')
+    else:
+      f = open(fname, 'wb')
+    f.write(content)
+
+  def name_gen():
+    i = 1
+    while True:
+      yield str(i)
+      i += 1
+  name_it = name_gen()
+
+  m = msg
+  title = decode_header(m['Subject'])
+  mailtype = m.get_content_type()
+  if mailtype == 'multipart/alternative':
+    mainMail = [m for m in m.get_payload()
+                if m.get_content_type() == 'text/html'][0]
+    mailbody = decode_payload(mainMail)
+  elif mailtype in ('multipart/related', 'multipart/mixed'):
+    mails = m.get_payload()
+    cidMapping = {}
+    for mail in mails:
+      if mail.get_content_type() == 'multipart/alternative':
+        mainMail = [m for m in mail.get_payload()
+                    if m.get_content_type() == 'text/html'][0]
+        mailbody = decode_payload(mainMail)
+      elif mail.get_content_type().startswith('text/html'):
+        mailbody = decode_payload(mail)
+      else:
+        try:
+          cid = mail['Content-ID'][1:-1]
+        except TypeError:
+          if mail['Content-Disposition'] and \
+             mail['Content-Disposition'].find('attachment') != -1:
+            continue
+          raise
+        fname = decode_header(mail.get_filename() or next(name_it))
+        cidMapping[cid] = fname
+        body = decode_payload(mail, binary=True)
+        save_file(fname, body)
+  elif mailtype == 'text/html':
+    mailbody = decode_payload(m)
+  else:
+    raise NotImplementedError('type %s not recognized' % mailtype)
+
+  from lxml.html import fromstring, tostring
+  from lxml.html import builder as E
+
+  div = fromstring(mailbody)
+  for cidLink in div.xpath('//*[starts-with(@src, "cid:")]'):
+    cid = cidLink.get('src')[4:]
+    cidLink.set('src', cidMapping[cid])
+  div.insert(0, E.TITLE(title))
+  div.insert(0, E.META(charset='utf-8'))
+  mailbody_b = tostring(div, encoding='utf-8')
+  save_file('index.html', mailbody_b)
+
+  return os.path.join(basedir, 'index.html')
+
