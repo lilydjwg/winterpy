2e6e79bb46c9bd1799af3988b630f331bbc3b601
lilydjwg <lilydjwg@gmail.com>
2019-01-02 16:30:51 +0800

    QQWry: optimize, remove all seeks and tells

--- pylib/QQWry.py
+++ pylib/QQWry.py
@@ -6,12 +6,13 @@
 作者 AutumnCat. 最后修改在 2008年 04月 29日
 bones7456 最后修改于 2009-02-02
 lilydjwg 修改于 2014-05-26
+lilydjwg 再次修改于 2019-01-02
 本程序遵循 GNU GENERAL PUBLIC LICENSE Version 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
 '''
 
 from struct import unpack, pack
 import sys
-import _socket, mmap
+import socket, mmap
 from collections import namedtuple
 import re
 import os
@@ -19,6 +20,7 @@ import zlib
 import subprocess
 import tempfile
 import shutil
+from typing import Tuple
 
 from myutils import safe_overwrite
 
@@ -30,18 +32,17 @@ data_url = 'http://update.cz88.net/ip/qqwry.rar'
 def _ip2ulong(ip):
   '''点分十进制 -> unsigned long
   '''
-  return unpack('>L', _socket.inet_aton(ip))[0]
+  return unpack('>L', socket.inet_aton(ip))[0]
 
 def _ulong2ip(ip):
   '''unsigned long -> 点分十进制
   '''
-  return _socket.inet_ntoa(pack('>L', ip))
+  return socket.inet_ntoa(pack('>L', ip))
 
 def _extract_date(s):
     return tuple(int(x) for x in re.findall(r'\d+', s))
 
-class ipInfo(namedtuple('ipInfo', 'sip eip country area')):
-  __slots__ = ()
+class IpInfo(namedtuple('IpInfo', 'sip eip country area')):
   def __str__(self):
     '''str(x)
     '''
@@ -55,32 +56,32 @@ class ipInfo(namedtuple('ipInfo', 'sip eip country area')):
       _ulong2ip(self[0]), _ulong2ip(self[1]), self[2], self[3])
 
 class QQWry:
-  def __init__(self, dbfile = DataFileName, charset = 'gbk'):
+  def __init__(self, dbfile=DataFileName, charset='gbk'):
     if isinstance(dbfile, (str, bytes)):
       dbfile = open(dbfile, 'rb')
 
-    self.f = dbfile
     self.charset = charset
-    self.f.seek(0)
-    self.indexBaseOffset = unpack('<L', self.f.read(4))[0] #索引区基址
-    self.Count = (unpack('<L', self.f.read(4))[0] - self.indexBaseOffset) // 7 # 索引数-1
+    self.f = mmap.mmap(dbfile.fileno(), 0, access=mmap.MAP_SHARED)
+    self.indexBaseOffset = unpack('<L', self.f[0:4])[0] #索引区基址
+    self.count = (unpack('<L', self.f[4:8])[0]
+                  - self.indexBaseOffset) // 7 # 索引数-1
 
-  def Lookup(self, ip):
+  def Lookup(self, ip: str) -> IpInfo:
     '''x.Lookup(ip) -> (sip, eip, country, area) 查找 ip 所对应的位置.
 
     ip, sip, eip 是点分十进制记录的 ip 字符串.
     sip, eip 分别是 ip 所在 ip 段的起始 ip 与结束 ip.
     '''
-    return self.nLookup(_ip2ulong(ip))
+    return self._n_lookup(_ip2ulong(ip))
 
-  def nLookup(self, ip):
-    '''x.nLookup(ip) -> (sip, eip, country, area) 查找 ip 所对应的位置.
+  def _n_lookup(self, ip: int) -> IpInfo:
+    '''x._n_lookup(ip) -> (sip, eip, country, area) 查找 ip 所对应的位置.
 
     ip 是 unsigned long 型 ip 地址.
     其它同 x.Lookup(ip).
     '''
     si = 0
-    ei = self.Count
+    ei = self.count
     if ip < self._readIndex(si)[0]:
       raise LookupError('IP NOT Found.')
     elif ip >= self._readIndex(ei)[0]:
@@ -94,7 +95,7 @@ class QQWry:
           ei = mi
     ipinfo = self[si]
     if ip > ipinfo[1]:
-      raise LookupError('IP NOT Found.')
+      raise LookupError('IP not found.')
     else:
       return ipinfo
 
@@ -103,114 +104,78 @@ class QQWry:
     tmp.append('RecCount:')
     tmp.append(str(len(self)))
     tmp.append('\nVersion:')
-    tmp.extend(self[self.Count].normalize()[2:])
+    tmp.extend(self[self.count].normalize()[2:])
     return ''.join(tmp)
 
   def __len__(self):
-    '''len(x)
-    '''
-    return self.Count + 1
+    return self.count + 1
 
   def __getitem__(self, key):
     '''x[key]
 
-    若 key 为整数, 则返回第key条记录(从0算起, 注意与 x.nLookup(ip) 不一样).
+    若 key 为整数, 则返回第key条记录(从0算起, 注意与 x._n_lookup(ip) 不一样).
     若 key 为点分十进制的 ip 描述串, 同 x.Lookup(key).
     '''
     if isinstance(key, int):
-      if key >=0 and key <= self.Count:
+      if key >= 0 and key <= self.count:
         index = self._readIndex(key)
         sip = index[0]
-        self.f.seek(index[1])
-        eip = unpack('<L', self.f.read(4))[0]
-        country, area = self._readRec()
+        pos = index[1]
+        eip = unpack('<L', self.f[pos:pos+4])[0]
+        country, area = self._readRec(pos+4)
         if area == ' CZ88.NET':
           area = ''
-        return ipInfo(sip, eip, country, area)
+        return IpInfo(sip, eip, country, area)
       else:
         raise KeyError('INDEX OUT OF RANGE.')
     elif isinstance(key, str):
       return self.Lookup(key).normalize()
     else:
-      raise TypeError('WRONG KEY TYPE.')
+      raise TypeError('key must be str or int')
 
-  def _read3ByteOffset(self):
-    '''_read3ByteOffset() -> unsigned long 从文件 f 读入长度为3字节的偏移.
-    '''
-    return unpack('<L', self.f.read(3) + b'\x00')[0]
+  def _read3ByteOffset(self, pos: int) -> int:
+    data = self.f[pos:pos+3] + b'\x00'
+    return unpack('<L', data)[0]
 
-  def _readCStr(self):
-    if self.f.tell() == 0:
-      return 'Unknown'
+  def _read_cstring(self, start: int) -> Tuple[str, int]:
+    if start == 0:
+      return 'Unknown', start
 
-    return self._read_cstring().decode(self.charset, errors='replace')
+    end = self.f.find(b'\x00', start)
+    if end < 0:
+      raise Exception('fail to read C string')
+    data = self.f[start:end]
+    return data.decode(self.charset, errors='replace'), end
 
-  def _read_cstring(self):
-    tmp = []
-    ch = self.f.read(1)
-    while ch != b'\x00':
-      tmp.append(ch)
-      ch = self.f.read(1)
-    return b''.join(tmp)
+  def _readIndex(self, n: int) -> Tuple[int, int]:
+    pos = self.indexBaseOffset + 7 * n
+    data = self.f[pos:pos+7] + b'\x00'
+    ip, offset = unpack('<LL', data)
+    return ip, offset
 
-  def _readIndex(self, n):
-    '''x._readIndex(n) -> (ip ,offset) 读取第n条索引.
-    '''
-    self.f.seek(self.indexBaseOffset + 7 * n)
-    return unpack('<LL', self.f.read(7) + b'\x00')
-
-  def _readRec(self, onlyOne=False):
-    '''x._readRec() -> (country, area) 读取记录的信息.
-    '''
-    mode = unpack('B', self.f.read(1))[0]
+  def _readRec(self, pos: int, *, onlyOne=False):
+    f = self.f
+    mode = f[pos]
     if mode == 0x01:
-      rp = self._read3ByteOffset()
-      bp = self.f.tell()
-      self.f.seek(rp)
-      result = self._readRec(onlyOne)
-      self.f.seek(bp)
+      rp = self._read3ByteOffset(pos+1)
+      result = self._readRec(rp, onlyOne=onlyOne)
     elif mode == 0x02:
-      rp = self._read3ByteOffset()
-      bp = self.f.tell()
-      self.f.seek(rp)
-      result = self._readRec(True)
-      self.f.seek(bp)
+      rp = self._read3ByteOffset(pos+1)
+      result = self._readRec(rp, onlyOne=True)
       if not onlyOne:
-        result.append(self._readRec(True)[0])
+        result.append(self._readRec(pos+4, onlyOne=True)[0])
     else: # string
-      self.f.seek(-1,1)
-      result = [self._readCStr()]
+      s, new_pos = self._read_cstring(pos)
+      result = [s]
       if not onlyOne:
-        result.append(self._readRec(True)[0])
+        result.append(self._readRec(new_pos, onlyOne=True)[0])
 
     return result
 
   def getDate(self):
-    return _extract_date(self[self.Count].area)
+    return _extract_date(self[self.count].area)
 
-class MQQWry(QQWry):
-  '''
-  将数据库放到内存
-  查询速度大约快两倍.
-
-  In [6]: %timeit t(QQWry())
-  100 loops, best of 3: 4.09 ms per loop
-
-  In [7]: %timeit t(MQQWry())
-  100 loops, best of 3: 2.22 ms per loop
-  '''
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.f = mmap.mmap(self.f.fileno(), 0, access = 1)
-
-  def _read_cstring(self):
-    start = self.f.tell()
-    end = self.f.find(b'\x00')
-    if end < 0:
-      raise Exception('fail to read C string')
-    self.f.seek(end + 1)
-    return self.f[start:end]
-
+MQQWry = QQWry
 
 def decipher_data(key, data):
   h = bytearray()
@@ -236,7 +201,7 @@ def update(q):
     tmp_dir = tempfile.mkdtemp(prefix='QQWry')
     old_d = os.getcwd()
     try:
-      Q = MQQWry()
+      Q = QQWry()
     except OSError as e:
       print('注意：原数据文件无法打开：', e, file=sys.stderr)
       Q = None
@@ -264,10 +229,10 @@ def update(q):
 
     os.chdir(old_d)
     safe_overwrite(DataFileName, d, mode='wb')
-    old_c = Q and Q.Count or 0
-    Q = MQQWry()
+    old_c = Q and Q.count or 0
+    Q = QQWry()
     if q != 2:
-      print('已经更新！数据条数 %d->%d.' % (old_c, Q.Count), file=sys.stderr)
+      print('已经更新！数据条数 %d->%d.' % (old_c, Q.count), file=sys.stderr)
   finally:
     shutil.rmtree(tmp_dir)
 
@@ -296,7 +261,7 @@ def main():
     update(q)
     return
 
-  Q = MQQWry()
+  Q = QQWry()
   if args.all:
     try:
       for i in Q: #遍历示例代码
