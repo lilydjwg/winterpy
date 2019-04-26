8af41de1cdaa826e84fd3e762363850da35c67b7
lilydjwg <lilydjwg@gmail.com>
2019-04-26 17:00:05 +0800

    github: fix commenting and adding labels don't work

--- pylib/github.py
+++ pylib/github.py
@@ -27,8 +27,16 @@ class GitHub(requestsutils.RequestsBase):
 
     if data:
       kwargs['json'] = data
+      if method == 'get':
+        method = 'post'
 
     return self.request(path, method=method, *args, **kwargs)
+
+  def get_issue(self, repo: str, issue_nr: int) -> 'Issue':
+    r = self.api_request(
+      f'/repos/{repo}/issues/{issue_nr}')
+    j = r.json()
+    return Issue(j, self)
 
   def get_repo_issues(self, repo, *, state='open', labels=''):
     params = {'state': state}
