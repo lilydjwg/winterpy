6aa23097633f3a3dc34384e3831bd137e06c597f
lilydjwg <lilydjwg@gmail.com>
2020-10-24 17:54:21 +0800

    tldextractutils: adapt to tldextract 3.0.1

--- pylib/tldextractutils.py
+++ pylib/tldextractutils.py
@@ -8,32 +8,25 @@ import time
 from xdg.BaseDirectory import save_cache_path
 import tldextract
 
-_extractors = {}
-
 def extract(url, *, include_psl_private_domains=False):
-  extractor = _extractors.get(include_psl_private_domains)
-  if extractor is None:
-    if include_psl_private_domains:
-      tld_data = os.path.join(
-        save_cache_path('tldextract'), 
-        'tld_data_psl'
-      )
-    else:
-      tld_data = os.path.join(
-        save_cache_path('tldextract'), 
-        'tld_data'
-      )
-    extractor = tldextract.TLDExtract(
-      include_psl_private_domains = include_psl_private_domains,
-    )
-    extractor.cache_file = tld_data
-    try:
-      t = os.path.getmtime(tld_data)
-      if time.time() - t > 86400 * 7:
-        extractor.update()
-    except FileNotFoundError:
-      pass
-    _extractors[include_psl_private_domains] = extractor
+  cache_dir = save_cache_path('tldextract')
+  last_updated = os.path.join(cache_dir, 'last_updated')
+  extractor = tldextract.TLDExtract(
+    cache_dir = cache_dir,
+    include_psl_private_domains = include_psl_private_domains,
+  )
+
+  update = False
+  try:
+    t = os.path.getmtime(last_updated)
+    if time.time() - t > 86400 * 7:
+      update = True
+  except FileNotFoundError:
+    update = True
+
+  if update:
+    extractor.update()
+    with open(last_updated, 'w'): pass
 
   return extractor(url)
 
