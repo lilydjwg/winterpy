171e2df6b4d7a8bd62969c359fcf79089a876aff
lilydjwg <lilydjwg@gmail.com>
2020-12-05 16:13:08 +0800

    mwutils: add search_and_replace function

--- pylib/mwutils.py
+++ pylib/mwutils.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
-
 import re
+import logging
+
+import mwclient # type: ignore
+
+logger = logging.getLogger(__name__)
 
 def move_category(site, old, new):
   cat = site.pages['Category:' + old]
@@ -13,3 +16,17 @@ def move_category(site, old, new):
       continue
     p.save(text, '分类重命名：[[Category:%s]] -> [[Category:%s]]' % (
       old, new), minor=True)
+
+def search_and_replace(
+  site: mwclient.Site, search: str, regex: re.Pattern, replace: str,
+  namespace: int = 0,
+) -> None:
+  for result in site.search(search, namespace=namespace, what='text'):
+    page = site.pages[result['title']]
+    text = page.text()
+    newtext = regex.sub(replace, text)
+    if newtext != text:
+      page.edit(newtext, summary=f'replace {regex.pattern} with {replace} (auto)')
+      logger.info('page %s updated.', page.page_title)
+    else:
+      logger.warning('page %s not updated.', page.page_title)
