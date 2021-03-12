9c76e00b2d6fb020aad0f504d65b37a43b516079
lilydjwg <lilydjwg@gmail.com>
2021-03-12 14:03:07 +0800

    graphiteutils: reconnect once on error

--- pylib/graphiteutils.py
+++ pylib/graphiteutils.py
@@ -7,12 +7,20 @@ class Graphite:
   def __init__(self, host, port=2003):
     self.host = host
     self.port = port
-    self.sock = sock = socket.socket()
-    sock.connect((host, port))
+    self._connect()
+
+  def _connect(self):
+    self.sock = socket.socket()
+    self.sock.connect((self.host, self.port))
 
   def send_stats(self, data):
     logger.info('sending %d metrics to Graphite %s:%s',
                 len(data), self.host, self.port)
     data = [x.encode() + b'\n' for x in data]
     data = b''.join(data)
-    self.sock.sendall(data)
+    try:
+      self.sock.sendall(data)
+    except ConnectionResetError:
+      # retry e.g. server has restarted
+      self._connect()
+      self.sock.sendall(data)
