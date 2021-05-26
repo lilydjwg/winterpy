a953730c0f3c5c2d48ce70d452f43c7a0df51cb2
lilydjwg <lilydjwg@gmail.com>
2021-05-26 15:09:21 +0800

    expiringdict: add set_item method

--- pylib/expiringdict.py
+++ pylib/expiringdict.py
@@ -2,9 +2,9 @@ from collections import UserDict
 import time
 
 class ExpiringDict(UserDict):
-  def __init__(self, expiration, maxsize=100):
+  def __init__(self, default_ttl, maxsize=100):
     super().__init__()
-    self.expiration = expiration
+    self.default_ttl = default_ttl
     self.maxsize = maxsize
 
   def __getitem__(self, key):
@@ -12,15 +12,20 @@ class ExpiringDict(UserDict):
     return item
 
   def __setitem__(self, key, value):
-    t = time.time()
+    self.set_item[key] = value
+
+  def set_item(self, key, value, ttl=None):
+    if ttl is None:
+      ttl = self.default_ttl
+    t = time.time() + ttl
     self.data[key] = value, t
 
   def __delitem__(self, key):
     del self.data[key]
 
   def expire(self):
-    deadline = time.time() - self.expiration
-    expired_keys = [k for k, (_, t) in self.data.items() if t < deadline]
+    now = time.time()
+    expired_keys = [k for k, (_, t) in self.data.items() if t < now]
     for k in expired_keys:
       del self.data[k]
 
