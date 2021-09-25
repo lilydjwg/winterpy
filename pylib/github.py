271dc3c6b6ce461aca4bdb29920d7b5075ef0481
lilydjwg <lilydjwg@gmail.com>
2021-09-25 17:23:32 +0800

    github: add Issue.is_pull

--- pylib/github.py
+++ pylib/github.py
@@ -83,6 +83,7 @@ class Issue:
     self.title = data['title']
     self.labels = [x['name'] for x in data['labels']]
     self.updated_at = parse_datetime(data['updated_at'])
+    self.is_pull = bool(data.get('pull_request'))
     self._api_url = f"{data['repository_url']}/issues/{data['number']}"
 
   def comment(self, comment: str) -> Response:
