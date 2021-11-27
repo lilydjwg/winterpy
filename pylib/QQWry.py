487ecbef598b37ced4a322a6c0d8986a491ef71e
lilydjwg <lilydjwg@gmail.com>
2021-11-27 18:14:17 +0800

    QQWry: use IPv4

--- pylib/QQWry.py
+++ pylib/QQWry.py
@@ -205,7 +205,7 @@ def update(q):
       Q = None
     os.chdir(tmp_dir)
 
-    wget = ['wget', '-U', 'Mozilla/3.0 (compatible; Indy Library)']
+    wget = ['wget', '-4', '-U', 'Mozilla/3.0 (compatible; Indy Library)']
     if q:
       wget.append('-q')
     subprocess.run(wget + [copywrite_url], check=True)
@@ -220,7 +220,7 @@ def update(q):
     else:
       if q != 2:
         print(info['text'], '开始下载...', file=sys.stderr, flush=True)
-    p = subprocess.Popen(['wget', '-U', 'Mozilla/3.0 (compatible; Indy Library)', data_url])
+    p = subprocess.Popen(['wget', '-4', '-U', 'Mozilla/3.0 (compatible; Indy Library)', data_url])
     p.wait()
     d = open('qqwry.rar', 'rb').read()
     d = decipher_data(info['key'], d)
