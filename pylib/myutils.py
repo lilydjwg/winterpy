cfd7538bd982eadc4f8c4d4755e825b98b8405c5
lilydjwg <lilydjwg@gmail.com>
2022-04-04 14:11:26 +0800

    file_lock: add type hints

--- pylib/myutils.py
+++ pylib/myutils.py
@@ -305,7 +305,7 @@ def lock_file(path: os.PathLike) -> None:
     fcntl.flock(lock, fcntl.LOCK_EX)
 
 @contextlib.contextmanager
-def file_lock(file):
+def file_lock(file: os.PathLike) -> Generator[None, None, None]:
   lock = os.open(file, os.O_WRONLY | os.O_CREAT, 0o600)
   try:
     fcntl.flock(lock, fcntl.LOCK_EX)
