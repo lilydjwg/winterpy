5ed414d1f588a84c4730be819b3f9d339a9f9818
lilydjwg <lilydjwg@gmail.com>
2022-09-30 15:44:28 +0800

    netutils: add get_self_ip_akamai and get_self_ip_google

--- pylib/netutils.py
+++ pylib/netutils.py
@@ -41,3 +41,34 @@ def get_gateway_ipv4() -> Optional[str, str]:
         return iface, gateway
 
   return None, None
+
+AKAMAI_NS_SERVERS = ['193.108.91.133', '184.85.248.64', '2.16.130.66']
+def get_self_ip_akamai(server):
+  import subprocess
+  lines = subprocess.check_output(
+    ['dig', 'whoami.ipv4.akahelp.net', 'TXT', f'@{server}', '+short'],
+    stderr = subprocess.DEVNULL,
+    text = True,
+  ).splitlines()
+
+  for line in lines:
+    if line.startswith(';;'):
+      continue
+    if line.startswith('"ns" '):
+      return line.split()[-1].strip('"')
+
+GOOGLE_NS_SERVERS = ['216.239.32.10', '216.239.34.10']
+def get_self_ip_google(server):
+  import subprocess
+  lines = subprocess.check_output(
+    ['dig', 'o-o.myaddr.l.google.com', 'TXT', f'@{server}', '+short'],
+    stderr = subprocess.DEVNULL,
+    text = True,
+  ).splitlines()
+
+  for line in lines:
+    if line.startswith(';;'):
+      continue
+    if line.startswith('"'):
+      return line.strip('"')
+
