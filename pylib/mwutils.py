bb3ebfb3c638dbe867a203e5d7db883220546968
lilydjwg <lilydjwg@gmail.com>
2023-03-01 13:46:35 +0800

    mwutils: use IntEnum for namespace

--- pylib/mwutils.py
+++ pylib/mwutils.py
@@ -1,9 +1,29 @@
 import re
 import logging
+from typing import Optional
+import enum
 
 import mwclient # type: ignore
 
 logger = logging.getLogger(__name__)
+
+class Namespace(enum.IntEnum):
+  MAIN = 0
+  TALK = 1
+  USER = 2
+  USER_TALK = 3
+  PROJECT = 4
+  PROJECT_TALK = 5
+  FILE = 6
+  FILE_TALK = 7
+  MEDIAWIKI = 8
+  MEDIAWIKI_TALK = 9
+  TEMPLATE = 10
+  TEMPLATE_TALK = 11
+  HELP = 12
+  HELP_TALK = 13
+  CATEGORY = 14
+  CATEGORY_TALK = 15
 
 def move_category(site, old, new):
   cat = site.pages['Category:' + old]
@@ -19,14 +39,23 @@ def move_category(site, old, new):
 
 def search_and_replace(
   site: mwclient.Site, search: str, regex: re.Pattern, replace: str,
-  namespace: int = 0,
+  namespace: Namespace = Namespace.MAIN,
+  summary: Optional[str] = None,
+  minor: bool = False,
+  limit: Optional[int] = None,
 ) -> None:
-  for result in site.search(search, namespace=namespace, what='text'):
+  count = 0
+  if summary is None:
+    summary = f'replace {regex.pattern} with {replace} (bot)'
+  for result in site.search(search, namespace=namespace.value, what='text'):
     page = site.pages[result['title']]
     text = page.text()
     newtext = regex.sub(replace, text)
     if newtext != text:
-      page.edit(newtext, summary=f'replace {regex.pattern} with {replace} (auto)')
+      page.edit(newtext, summary=summary, minor=minor)
       logger.info('page %s updated.', page.page_title)
+      count += 1
     else:
       logger.warning('page %s not updated.', page.page_title)
+    if limit and count >= limit:
+      break
