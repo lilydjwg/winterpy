0cbe14f4ba9cad4b711acdc1041eaa00511d3015
lilydjwg <lilydjwg@gmail.com>
2024-10-06 17:23:14 +0800

    mailerrorlog: aggreate logs instead of delaying them

--- pylib/mailerrorlog.py
+++ pylib/mailerrorlog.py
@@ -1,21 +1,27 @@
 from __future__ import annotations
 
 import logging
-from typing import List, Tuple, Deque
+from typing import Tuple, Deque
 from collections import deque
 from collections.abc import Sequence
-import time
+import threading
+from queue import Queue, Empty
 
-from mailutils import assemble_mail, sendmail
+from .mailutils import assemble_mail, sendmail
 
-class LocalSMTPHandler(logging.Handler):
+class LogMailSender(threading.Thread):
+  name = "LogMailSender"
+  daemon = True
+
   def __init__(
     self,
+    queue: Queue,
+    handler: LocalSMTPHandler,
     fromaddr: str,
-    toaddrs: List[str],
+    toaddrs: list[str],
     /,
     tag: str,
-    min_gap_seconds: int,
+    delay: int,
     *,
     max_num: int = 10,
   ) -> None:
@@ -25,16 +31,28 @@ class LocalSMTPHandler(logging.Handler):
       toaddrs = [toaddrs]
     self.toaddrs = toaddrs
     self.tag = tag
-    self.min_gap_seconds = min_gap_seconds
+    self.delay = delay
 
     self.last_mail_time = 0.0
     self.deque: Deque[logging.LogRecord] = deque(maxlen = max_num)
 
-  def emit(self, record: logging.LogRecord) -> None:
-    self.deque.append(record)
+    self.queue = queue
+    self.handler = handler
 
-    t = time.time()
-    if t <= self.last_mail_time + self.min_gap_seconds:
+  def run(self):
+    while True:
+      self.run_one()
+
+  def run_one(self):
+    try:
+      record = self.queue.get(timeout=self.delay)
+      self.deque.append(record)
+      if len(self.deque) < self.deque.maxlen:
+        return
+    except Empty:
+      pass
+
+    if not self.deque:
       return
 
     subject, body = self.format_as_mail(self.deque)
@@ -47,9 +65,8 @@ class LocalSMTPHandler(logging.Handler):
       )
       sendmail(mail)
       self.deque.clear()
-      self.last_mail_time = t
     except Exception:
-      self.handleError(record)
+      self.handler.handleError(record)
 
   def format_as_mail(self, records: Sequence[logging.LogRecord]) -> Tuple[str, str]:
     if len(records) == 1:
@@ -60,8 +77,31 @@ class LocalSMTPHandler(logging.Handler):
     ret = []
 
     for record in records:
-      formatted = self.format(record)
+      formatted = self.handler.format(record)
       ret.append(formatted)
 
     return subject, '\n'.join(ret) + '\n'
 
+class LocalSMTPHandler(logging.Handler):
+  def __init__(
+    self,
+    fromaddr: str,
+    toaddrs: list[str],
+    /,
+    tag: str,
+    delay: int,
+    *,
+    max_num: int = 10,
+  ) -> None:
+    super().__init__()
+    self.queue: Queue[logging.LogRecord] = Queue(maxsize=1)
+
+    self.worker = LogMailSender(
+      self.queue, self,
+      fromaddr, toaddrs,
+      tag = tag, delay = delay, max_num = max_num,
+    )
+    self.worker.start()
+
+  def emit(self, record: logging.LogRecord) -> None:
+    self.queue.put(record)
