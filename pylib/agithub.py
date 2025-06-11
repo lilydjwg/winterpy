3a3ddf45b0a3db5274255268bed1a2d03a505fc1
lilydjwg <lilydjwg@gmail.com>
2025-06-11 17:13:06 +0800

    agithub: fix httpxutils usage

--- pylib/agithub.py
+++ pylib/agithub.py
@@ -56,17 +56,17 @@ class GitHub(httpxutils.ClientBase):
     for _ in range(3):
       res = await self.request(path, method=method, **kwargs)
       j: JsonDict
-      if res.status == 204:
+      if res.status_code == 204:
         j = {}
       else:
         j = await res.json()
         if 'message' in j:
-          if res.status == 403 and int(res.headers.get('X-RateLimit-Remaining', -1)) == 0:
+          if res.status_code == 403 and int(res.headers.get('X-RateLimit-Remaining', -1)) == 0:
             reset = int(res.headers['X-RateLimit-Reset']) - time.time() + 1
             logger.warn('rate limited; sleeping for %ds: %s', reset, j['message'])
             await asyncio.sleep(reset)
             continue
-          raise GitHubError(j['message'], j['documentation_url'], res.status)
+          raise GitHubError(j['message'], j['documentation_url'], res.status_code)
       return j, res
 
     raise Exception('unreachable')
