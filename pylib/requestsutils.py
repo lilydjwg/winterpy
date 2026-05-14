14c02e8b622fac191c046686ac962286ff253643
lilydjwg <lilydjwg@gmail.com>
2026-05-14 16:11:26 +0800

    requestsutils: revert to requests
    niquests attempts to modify other package's files.

--- pylib/requestsutils.py
+++ pylib/requestsutils.py
@@ -3,11 +3,11 @@ from http.cookiejar import MozillaCookieJar
 from urllib.parse import urljoin
 from typing import Optional, BinaryIO
 
-import niquests
+import requests
 
 CHUNK_SIZE = 40960
 
-def download_into(session: niquests.Session,
+def download_into(session: requests.Session,
                   url: str, file: BinaryIO, process_func=None) -> None:
   r = session.get(url, stream=True)
   length = int(r.headers.get('Content-Length') or 0)
@@ -27,7 +27,7 @@ def download_into_with_progressbar(url, dest):
 
   w = os.get_terminal_size()[1]
   with open(dest, 'wb') as f:
-    download_into(niquests, url, f, partial(
+    download_into(requests, url, f, partial(
       download_process, dest, time.time(), width=w))
 
 class RequestsBase:
@@ -41,7 +41,7 @@ class RequestsBase:
   @property
   def session(self):
     if not self._session:
-      s = niquests.Session(resolver='dou://127.0.0.1', happy_eyeballs=True)
+      s = requests.Session()
       self.__our_session = True
       self._session = s
     return self._session
@@ -71,7 +71,7 @@ class RequestsBase:
       self._session.close()
 
   def request(self, url: str, method: Optional[str] = None, *args, **kwargs
-             ) -> niquests.Response:
+             ) -> requests.Response:
     if self.baseurl:
       url = urljoin(self.baseurl, url)
 
