b3f56bd80927dc62c73767ba7d9ca8a02f939f23
lilydjwg <lilydjwg@gmail.com>
2026-06-29 11:13:22 +0800

    llama-*, yt-summarize: move more common logic to a module

--- pylib/myopenai.py
+++ pylib/myopenai.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 def print_streaming_response(r):
   ret = []
@@ -53,3 +54,108 @@ def interact(client, url, model, messages):
         'role': 'assistant',
         'content': ''.join(res),
       })
+
+from typing import Literal, Any, get_args
+import os
+from contextlib import AbstractContextManager
+
+import httpx
+
+API_TYPE = Literal['local', 'gemini']
+API_CHOICES = get_args(API_TYPE)
+API_ENDPOINTS = {
+  'local': 'http://127.0.0.1:8080/v1/chat/completions',
+  'gemini': 'https://generativelanguage.googleapis.com/v1beta/openai/chat/completions',
+}
+Messages = list[dict[str, Any]]
+
+class App:
+  def __init__(self, api: API_TYPE, model: str) -> None:
+    self.url = API_ENDPOINTS[api]
+    self.model = model
+    self.api = api
+
+    if api == 'gemini':
+      headers = {
+        'Authorization': f'Bearer {os.environ['GEMINI_API_KEY']}',
+      }
+    else:
+      headers = {}
+    self.client = httpx.Client(http2=True, headers=headers)
+
+  def stream(
+    self,
+    messages: Messages,
+    *,
+    disable_thinking: bool = True,
+    extra: dict[str, Any] | None = None,
+  ) -> AbstractContextManager[httpx.Response]:
+    j = self._get_request_json(
+      messages,
+      streaming = True,
+      disable_thinking = disable_thinking,
+      extra = extra,
+    )
+    return self.client.stream('POST', self.url, json=j, timeout=120)
+
+  def request(
+    self,
+    messages: Messages,
+    *,
+    disable_thinking: bool = True,
+    extra: dict[str, Any] | None = None,
+  ) -> httpx.Response:
+    j = self._get_request_json(
+      messages,
+      streaming = False,
+      disable_thinking = disable_thinking,
+      extra = extra,
+    )
+    return self.client.post(self.url, json=j, timeout=120)
+
+  def _get_request_json(
+    self,
+    messages: Messages,
+    *,
+    streaming: bool,
+    disable_thinking: bool = True,
+    extra: dict[str, Any] | None = None,
+  ) -> dict[str, Any]:
+    j = {
+      'model': self.model,
+      'messages': messages,
+      'stream': streaming,
+    }
+
+    if disable_thinking:
+      if self.api == 'local':
+        j['chat_template_kwargs'] = {'enable_thinking': False}
+      elif self.api == 'gemini':
+        j['reasoning_effort'] = 'minimal'
+
+    if extra:
+      j.update(extra)
+
+    return j
+
+  def interact(self, messages: Messages) -> None:
+    interact(self.client, self.url, self.model, messages)
+
+  @staticmethod
+  def add_common_argument(
+    parser: argparse.ArgumentParser,
+    *,
+    default_api: API_TYPE,
+  ):
+    parser.add_argument('-m', '--model',
+                        help='name of model to use')
+    parser.add_argument('--api', choices=API_CHOICES,
+                        default=default_api,
+                        help=f'API service to use. [default: {default_api}]')
+
+def set_process_name(title: str) -> None:
+  try:
+    import setproctitle
+    setproctitle.setproctitle(title)
+  except ImportError:
+    pass
